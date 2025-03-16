4697649d60b658f031b6273ab6deec386617731f
Andrei Ciudacov <stauceni.android@gmail.com>
2025-03-16 16:13:41 +0200

    refactor program.cs

--- Program.cs
+++ Program.cs
@@ -5,45 +5,38 @@ using Microsoft.EntityFrameworkCore;
 using Radzen;
 using Serilog;
 
-namespace CarHaulingAnalytics;
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddTransient<WidgetDataService>();
+
+builder.Services.AddRadzenComponents();
+
+builder.Services.AddDbContext<AnalyticContext>(options =>
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"), options => options.EnableRetryOnFailure());
+    options.LogTo(Log.Information);
+}, ServiceLifetime.Transient);
 
-        builder.Services.AddRazorComponents()
-            .AddInteractiveServerComponents();
+var app = builder.Build();
 
-        builder.Services.AddTransient<WidgetDataService>();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
 
-        builder.Services.AddRadzenComponents();
+app.UseHttpsRedirection();
 
-        builder.Services.AddDbContext<AnalyticContext>(options =>
-        {
-            options.UseSqlServer("Server=tcp:carhaulinganalytics.database.windows.net,1433;Initial Catalog=CarHaulingAnalytics;Persist Security Info=False;User ID=carhauler;Password=cVb![0c!1K095P1X;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=90;", options => options.EnableRetryOnFailure());
-            options.LogTo(Log.Information);
-        }, ServiceLifetime.Transient);
-
-        var app = builder.Build();
-
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Error");
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseStaticFiles();
+app.UseStaticFiles();
         
 
-        app.MapRazorComponents<App>()
-            .AddInteractiveServerRenderMode();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
-        app.UseRouting();
-        app.UseAntiforgery();
+app.UseRouting();
+app.UseAntiforgery();
 
-        app.Run();
-    }
-}
+await app.RunAsync();
